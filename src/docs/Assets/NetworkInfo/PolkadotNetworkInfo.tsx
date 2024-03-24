ea255d66ea2cd2e2587e4aaa95f5ac6856d9ce5f
wirednkod <wirednkod@gmail.com>
2024-03-25 01:03:37 +0200

    fix deps

--- src/docs/Assets/NetworkInfo/PolkadotNetworkInfo.tsx
+++ src/docs/Assets/NetworkInfo/PolkadotNetworkInfo.tsx
@@ -6,7 +6,7 @@ import { SimpleEditor } from "../../lib/SimpleEditor";
 export const PolkadotNetworkInfo = () => {
   const code = `// Import Polkadot's information object.
 
-import { polkadot } from '@polkadot-ui/assets/ecosystem';
+import { polkadot } from '@polkadot-ui/assets/external';
 
 console.log(polkadot);
 `;
