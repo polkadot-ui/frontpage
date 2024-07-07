f196ce9a22bf347e6b15319299bfbd847c1f9775
wirednkod <wirednkod@gmail.com>
2024-07-07 14:17:55 +0300

    one more

--- src/docs/Experimental/Connect/main.tsx
+++ src/docs/Experimental/Connect/main.tsx
@@ -22,12 +22,12 @@ export const Doc = ({ folder, npm }: DocProps) => {
       <h4>Introduction</h4>
       <p>
         The <code>Wallets Connect</code> recipe (which actually is 2 specific
-        Providers - the <code>ExtensionProvider</code> which identifies the extensions
-        installed in the browser and the <code>AccountProvider</code> which is listing the
-        accounts from the selected extensions) is meant to exist for quick and
-        fast integration of any app with the installed wallets of the user's
-        browser. It identifies the installed extension and upon clicking on one,
-        it prompts the user to connect be shown.
+        Providers - the <code>ExtensionProvider</code> which identifies the
+        extensions installed in the browser and the <code>AccountProvider</code>{" "}
+        which is listing the accounts from the selected extensions) is meant to
+        exist for quick and fast integration of any app with the installed
+        wallets of the user's browser. It identifies the installed extension and
+        upon clicking on one, it prompts the user to connect be shown.
       </p>
       <ConnectPageSimple />
     </>
